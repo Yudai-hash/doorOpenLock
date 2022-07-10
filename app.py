92a955cd79b3e0bd16817c6f90981d2fc8253d4c
yudai <1yun2yun3yudai@gmail.com>
2022-07-10 09:05:02 +0900

    motor

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
+import RPi.GPIO as GPIO
+
+duty = 80
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+#GPIO.setup(27,GPIO.IN)
+
+
+
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('4wmhUDo4We9wDOTNtwj0ts+S7HIU/8iBEk92oAXXlVhXvhHWskJlfk+RlDKrTGIx81FzYUFIVHCuEmk+eBkWx1UF9Gu4UmIo4X+knQDT7YFNhUytmA5/jnhrz+gbRk0zw3rXO8l3TvrwaEDVkODhygdB04t89/1O/w1cDnyilFU=')
@@ -39,23 +51,31 @@ def callback():
 def handle_message(event):
     lineRes = event.message.text
     botRes = '鍵を施錠、開錠できます'
-
-    if lineRes == '123':
-        GPIO.output(4,1)
-        botRes = '開錠しました'
-    if lineRes == '456':
-        GPIO.output(4,0)
-        botRes = '施錠しました'
-    if lineRes == '789':
-        if GPIO.input(17):
-            botRes = '開錠中'
+    #   GPIO.output(27,True)
+    if lineRes == 'lock':
+        GPIO.output(4,True)
+        GPIO.output(17,False)
+        #p1.ChangeDutyCycle(duty)
+        #p2.ChangeDutyCycle(0)
+        botRes = 'LOCK'
+    elif lineRes == 'open':
+        GPIO.output(4,False)
+        GPIO.output(17,True)
+        #p1.ChangeDutyCycle(0)
+        #p2.ChangeDutyCycle(duty)
+        botRes = 'OPEN'
+    elif lineRes == 'check':
+        if GPIO.input(27):
+            botRes = 'CLOSE'
         else:
-            botRes = '施錠中'
+            botRes = 'OPEN'
     
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=botRes))
-
+    #Clean might be a problem to get "please set pin numbering mode using GPIO.
+    #.setmode(GPIO.BOARD) or GPIO.setmode(GPIO.BCM)"
+    #    GPIO.cleanup()
 
 if __name__ == "__main__":
     app.run()
